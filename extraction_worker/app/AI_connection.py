f4a5629099f627ac7f507eadfd9bd6c98a08b3d3
kobi <kodigershuni@gmail.com>
2026-03-23 14:54:57 +0200

    added try except for ai

--- extraction_worker/app/AI_connection.py
+++ extraction_worker/app/AI_connection.py
@@ -6,13 +6,17 @@ load_dotenv()
 
 
 def ask_ai(question):
-    client = genai.Client(
-        api_key=os.getenv("GEMINI_API_KEY")
+    try:
+        client = genai.Client(
+            api_key=os.getenv("GEMINI_API_KEY")
+            )
+        response = client.models.generate_content(
+            model="gemini-3-flash-preview", 
+            contents=question
         )
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview", 
-        contents=question
-    )
-    return response.text
+        return response.text
+    except Exception as e:
+        print(f"error: {e}")
+        return None
 
 print(ask_ai("What is the capital of France?"))
