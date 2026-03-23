2561069590c5cbe826a0974b0bdfd2746972bba7
Yosef shoval <ys0542522504@gmail.com>
2026-03-23 17:03:03 +0200

    change the schema so it fit the excepted message received from kafka

--- storageWorker/app/models/schemas.py
+++ storageWorker/app/models/schemas.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel, ConfigDict
-from typing import List, Dict, Any
+from typing import List
+
+class Item(BaseModel):
+    name: str
+    quantity: int
+    price: float
+    category: str
 
 class Receipt(BaseModel):
     model_config = ConfigDict(extra='allow')
+    payment_method: str
     receipt_id: str
-    items: List[Dict[str, Any]] = []
+    store: str
+    purchase_date: str
+    total_price: float
+    items: List[Item] = []
