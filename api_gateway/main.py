2f9e6f7a9955519ac7eeb5696a45bab7f72360e4
MenachemKazido <agbg7172@gmail.com>
2026-03-24 11:46:34 +0200

    finish api-gateway with docker

--- api_gateway/main.py
+++ api_gateway/main.py
@@ -2,10 +2,14 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 from fastapi import FastAPI
-from routers import router
+from api.routers import router
+from logging_system.logger import log_to_elastic
 
 app = FastAPI(title="API Gateway for Receipt Processing")
 
+@app.on_event("startup")
+async def startup_event():
+    log_to_elastic("INFO", "API Gateway service started")
 
 app.include_router(router)
 
--- api_gateway/models.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class ManualEntryRequest(BaseModel):
-    data: dict
