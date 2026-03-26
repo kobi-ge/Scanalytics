42fe6ed5e40c3f3bc248537f59dc6716f3deeb4f
Yosef shoval <ys0542522504@gmail.com>
2026-03-26 12:34:18 +0200

    allowed host in vite.config.js for openshift host

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -8,6 +7,8 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
+    // פתיחת האפשרות לגשת מכתובות חיצוניות ב-OpenShift
+    allowedHosts: true, 
     proxy: {
       "/api-gw": {
         target: "http://api-gateway:8000",
