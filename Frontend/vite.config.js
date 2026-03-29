6ff1c7f88937fff939773497b4bf651a9d210059
Yosef shoval <ys0542522504@gmail.com>
2026-03-29 15:38:35 +0300

    add allowlist to vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -8,6 +7,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
+    port: 5173,
+    // פותר את השגיאה Blocked request. This host is not allowed
+    allowedHosts: ["scanalytics.duckdns.org"], 
     proxy: {
       "/api-gw": {
         target: "http://api-gateway:8000",
@@ -23,6 +25,10 @@ export default defineConfig({
         target: "http://backend:3000",
         changeOrigin: true,
       },
+    },
+    // וודא ש-Vite מבין שהוא רץ מאחורי פרוקסי עבור ה-WebSockets
+    hmr: {
+      clientPort: 443,
     },
   },
 });
